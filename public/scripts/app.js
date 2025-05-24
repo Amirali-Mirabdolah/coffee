ffc7210a2d13d9038228d430924165e10571a477
amirali <amiralimirabdolah@gmail.com>
2025-05-25 02:39:27 +0330

    Responsive Cart for mobile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-const toggleThemeBtn = document.querySelector("#toggle-theme");
+// const toggleThemeBtn = document.querySelectorAll(".toggle-theme");
 const openSubmenuButton = document.querySelector('.submenu-open-btn')
 const submenu = document.querySelector('.submenu')
 const chevronIcon = document.querySelector('#submenu-chevron')
@@ -7,20 +7,18 @@ const overlay = document.querySelector('.overlay')
 const navMenu = document.querySelector('.nav-menu')
 const closeMenuButton = document.querySelector('.close-menu')
 
-const closeMenu = () => {
-    overlay.classList.toggle('hidden')
-    navMenu.classList.remove('-right-52')
-    navMenu.classList.add('right-0')
-}
+document.querySelectorAll('.toggle-theme').forEach(button => {
+    button.addEventListener("click", () => {
+        console.log('toggle clicked');
 
-toggleThemeBtn.addEventListener("click", () => {
-    if (localStorage.theme === "dark") {
-        document.documentElement.classList.remove("dark");
-        localStorage.theme = "light";
-    } else {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "dark");
-    }
+        if (localStorage.theme === "dark") {
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
+        }
+    })
 })
 
 openSubmenuButton.addEventListener('click', (e) => {
@@ -29,16 +27,22 @@ openSubmenuButton.addEventListener('click', (e) => {
     chevronIcon.classList.toggle('rotate-180')
 })
 
-barsIcon.addEventListener('click', closeMenu)
+const closeMenuHandler = () => {
+    navMenu.classList.remove('right-0')
+    navMenu.classList.add('-right-52')
+    overlay.classList.add('hidden')
+}
+
+barsIcon.addEventListener('click', () => {
+    navMenu.classList.add('right-0')
+    navMenu.classList.remove('-right-52')
+    overlay.classList.remove('hidden')
+})
+
+closeMenuButton.addEventListener('click', closeMenuHandler)
 
 overlay.addEventListener('click', (e) => {
     if (e.currentTarget) {
-        closeMenu()
-        navMenu.classList.add('-right-52')
+        closeMenuHandler()
     }
-})
-
-closeMenuButton.addEventListener('click', ()=>{
-    closeMenu()
-    navMenu.classList.add('-right-52')
 })
